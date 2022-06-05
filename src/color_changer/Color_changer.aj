7ad8af66d854772f9ee7c20bcd56865fce845560
rimacias <rimacias@espol.edu.ec>
2022-06-04 21:53:57 -0500

    Aspect Color Changer

---
+++ src/color_changer/Color_changer.aj
@@ -0,0 +1,8 @@
+package color_changer;
+
+public aspect Color_changer {
+	pointcut successRed() : call(* *Red(..));
+	after():
+		
+	
+}
--- src/color_game/Main.java
+++ src/color_game/Main.java
@@ -3,7 +3,9 @@ package color_game;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.fxml.FXMLLoader;
 
 
@@ -13,6 +15,11 @@ public class Main extends Application {
 		try {
 			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
 			Scene scene = new Scene(root,600,400);
+			Pane p = new Pane();
+			Button redBtn = new Button("Red");
+			
+			p.getChildren().add(redBtn);
+			root.getChildren().add(p);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
