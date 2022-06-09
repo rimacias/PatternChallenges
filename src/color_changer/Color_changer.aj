e16e4bf602491054cc66c8d8929d5444ee8af2a4
yanaleenpluas <85530151+yanaleenpluas@users.noreply.github.com>
2022-06-08 19:33:58 -0500

    FUNCIONALIDAD EXTRA
    Se implemento un boton SuccessMix que cada vez que se le da clic cambia de color de manera aleatoria

--- src/color_changer/Color_changer.aj
+++ src/color_changer/Color_changer.aj
@@ -1,29 +1,60 @@
 package color_changer;
 
+import java.util.concurrent.TimeUnit;
+
+import javafx.animation.PauseTransition;
+import javafx.concurrent.Task;
 import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
 
 public aspect Color_changer {
 	pointcut successRed(): call(* *Red(..));
+
 	after(): successRed(){
 		Object[] parameterList = thisJoinPoint.getArgs();
-		StackPane root = (StackPane) parameterList[0]; 
-		root.setStyle("-fx-background-color: #ff0000");
+		StackPane root = (StackPane) parameterList[0];
+		root.setStyle("-fx-background-color: red");
 		System.out.println("Cambiado a color rojo");
 	}
+
 	pointcut successGreen(): call(* *Green(..));
+
 	after(): successGreen(){
 		Object[] parameterList = thisJoinPoint.getArgs();
-		StackPane root = (StackPane) parameterList[0]; 
+		StackPane root = (StackPane) parameterList[0];
 		root.setStyle("-fx-background-color: green;");
 		System.out.println("Cambiado a color verde");
 	}
+
 	pointcut successBlue(): call(* *Blue(..));
+
 	after(): successBlue(){
 		Object[] parameterList = thisJoinPoint.getArgs();
-		StackPane root = (StackPane) parameterList[0]; 
+		StackPane root = (StackPane) parameterList[0];
 		root.setStyle("-fx-background-color: blue;");
-		System.out.println("Cambiado a color verde");
+		System.out.println("Cambiado a color azul");
 	}
+
+	pointcut successMix(): call(* *Mix(..));
+
+	after(): successMix(){
+		Object[] parameterList = thisJoinPoint.getArgs();
+		StackPane root = (StackPane) parameterList[0];
+		String[] colores = { "#c4fdd2", "#f1fc7f", "#EDFED3", "#79fcce", "#ffdfa9", "#fdb0a7", "#fc83a2" };
+		int al = (int) Math.round(Math.random() * (colores.length - 1));
+		String color_al = colores[al];
+		String s = "-fx-background-color: " + color_al + ";";
+		root.setStyle(s);
+
 		
-	
+	}
+
+	pointcut successStop(): call(* *Stop(..));
+
+	after(): successStop(){
+		Object[] parameterList = thisJoinPoint.getArgs();
+		StackPane root = (StackPane) parameterList[0];
+		root.setStyle("-fx-background-color: white;");
+		System.out.println("Cambiado a color blanco");
+	}
 }
--- src/color_game/Main.java
+++ src/color_game/Main.java
@@ -16,20 +16,21 @@ public class Main extends Application {
 	Button redBtn = new Button("Red");
 	Button greenBtn = new Button("Green");
 	Button blueBtn = new Button("Blue");
+	Button mixBtn = new Button("Mix it up");
+	Button stopBtn = new Button("Stop");
+	Boolean keepMixing = false;
 	HBox btnContainer = new HBox();
+	
 	@Override
 	public void start(Stage primaryStage) {
-		try {
-			prepare();
-			Scene scene = new Scene(root,600,400);
-			StackPane.setAlignment(btnContainer,Pos.CENTER);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setTitle("COLOR GAME");
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+		prepare();
+		Scene scene = new Scene(root,600,400);
+		StackPane.setAlignment(btnContainer,Pos.CENTER);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		primaryStage.setTitle("COLOR GAME");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+
 	}
 	
 	public static void main(String[] args) {
@@ -38,9 +39,12 @@ public class Main extends Application {
 	private void changeRed(StackPane root) {}
 	private void changeGreen(StackPane root) {}
 	private void changeBlue(StackPane root) {}
+	private void changeMix(StackPane root, boolean b) {}
+	private void colorStop(StackPane root) {}
 	public void prepare() {
-		btnContainer.getChildren().addAll(redBtn,greenBtn,blueBtn);
-		root.getChildren().add(btnContainer);
+		btnContainer.getChildren().addAll(redBtn,greenBtn,blueBtn,mixBtn,stopBtn);
+		btnContainer.setAlignment(Pos.TOP_CENTER);
+		root.getChildren().addAll(btnContainer);
 		redBtn.setOnAction(new EventHandler<>() {
 			@Override
 			public void handle(ActionEvent arg0) {
@@ -58,6 +62,18 @@ public class Main extends Application {
 			public void handle(ActionEvent arg0) {
 				changeGreen(root);
 			}					
+		});
+		mixBtn.setOnAction(new EventHandler<>() {
+			@Override
+			public void handle(ActionEvent arg0) {
+				changeMix(root, keepMixing);
+			}								
+		});
+		stopBtn.setOnAction(new EventHandler<>() {
+			@Override
+			public void handle(ActionEvent arg0) {
+				colorStop(root);
+			}								
 		});
 	}
 }
